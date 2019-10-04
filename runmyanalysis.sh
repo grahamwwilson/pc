745074e5c823bd7aeb6f0afd913af1a2f1361f9c
Graham Wilson <gwwilson@ku.edu>
2019-10-04 12:57:08 -0500

    update

--- runmyanalysis.sh
+++ runmyanalysis.sh
@@ -1,6 +1,14 @@
+#!/bin/sh
 
+date
+
+INPUTLIST=${1-data.list}
 
 #input is: num files, numthreads,  yourdata.list
 #0 is all files, optimal threads is 8
 #python runmacro.py 0 8 "bigmcdata.list"
-python runmacro.py 0 8 "bigdata.list"
+python runmacro.py 0 8 ${INPUTLIST}
+
+date
+
+exit
