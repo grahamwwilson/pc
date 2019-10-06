55ec465d7572d5d52ade3c0b1104353b66d0659e
graham <Graham.Wilson@cern.ch>
2019-10-06 17:38:24 -0500

    Fixes for local running

--- runmyanalysis.sh
+++ runmyanalysis.sh
@@ -7,7 +7,7 @@ INPUTLIST=${1-data.list}
 #input is: num files, numthreads,  yourdata.list
 #0 is all files, optimal threads is 8
 #python runmacro.py 0 8 "bigmcdata.list"
-python runmacro.py 0 8 ${INPUTLIST}
+python2 runmacro.py 0 8 ${INPUTLIST}
 
 date
 
