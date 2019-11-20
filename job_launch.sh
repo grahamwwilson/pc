0c602ecaeb043f42b8d317723558bb15281c8422
Wilson <gwwilson@ku.edu>
2019-11-20 17:31:32 -0600

    batch jobs on KU HPC cluster

---
+++ job_launch.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --job-name=conversions          # Job name
+#SBATCH --partition=sixhour             # Partition Name (Required)
+#SBATCH --mail-type=ALL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=gwwilson@ku.edu     # Where to send mail	
+#SBATCH --ntasks=1                      # Run 1 task on one node
+#SBATCH --cpus-per-task=16              # Number of threads to use
+#SBATCH --mem=2gb                       # Job memory request
+#SBATCH --time=0-06:00:00               # Time limit days-hrs:min:sec
+#SBATCH --output=conversions_%j.log     # Standard output and error log
+
+#VERSION=$1
+#echo 'Version '$VERSION
+
+pwd
+hostname
+date
+
+echo "PATH: "
+echo $PATH
+ 
+echo "LD_LIBRARY_PATH"
+echo $LD_LIBRARY_PATH
+
+echo "Run conversion code "
+
+python2 --version
+
+#Need to source ROOT
+
+. ~/setup.sh
+echo $ROOTSYS
+ 
+MYWDIR=/home/gwwilson/pc
+cd ${MYWDIR}
+echo "Now in directory "
+pwd
+
+echo "Start execution"
+
+./doboth.sh
+
+date
+
+
+
+date
+
+exit
--- runmyanalysis.sh
+++ runmyanalysis.sh
@@ -9,7 +9,7 @@ INPUTLIST=${INPUT}.list
 #input is: num files, numthreads,  yourdata.list
 #0 is all files, optimal threads is 8
 #python runmacro.py 0 8 "bigmcdata.list"
-python2 runmacro.py 0 8 ${INPUTLIST}
+python2 runmacro.py 0 16 ${INPUTLIST}
 
 mv Outfile.root PC_${INPUT}.root
 
