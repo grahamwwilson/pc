8c43528a2a39cc4e88b1adb20dcdf3efc75b45d8
graham <Graham.Wilson@cern.ch>
2020-04-12 17:10:23 -0500

    also for MC

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-sbatch job_launch.sh testdata_R1
-sbatch job_launch.sh testmc_R1
+#
+# By default ths now uses slimmed versions (MODE=1)      R1
+#
+# Specify MODE=0 to get the unslimmed version            R0
+# (this may still need some changes to the code - to check) 
+#
+MODE=${1-1}
+echo 'MODE set to '
+echo ${MODE}
+
+sbatch job_launch.sh testdata_R${MODE}
+sbatch job_launch.sh testmc_R${MODE}
 
 exit
