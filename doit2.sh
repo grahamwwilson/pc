0cc5013aa5dec33646318e52c54c7b19d63a55d4
graham <Graham.Wilson@cern.ch>
2020-04-26 15:28:40 -0500

    New jobs

---
+++ doit2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 3 data jobs
+#sbatch job_launch2.sh data0
+#sbatch job_launch2.sh data1
+sbatch job_launch2.sh data2
+
+# 3 MC jobs
+#sbatch job_launch2.sh mc0
+#sbatch job_launch2.sh mc1
+sbatch job_launch2.sh mc2
+
+exit
---
+++ gettest2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+scp -p gwwilson@hpc.crc.ku.edu:~/pc/PC_test*2.root .
+
+exit
