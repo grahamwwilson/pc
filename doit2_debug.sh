4ecc330e49de9ec55cf58253361318fcb3a2885a
graham <Graham.Wilson@cern.ch>
2020-06-06 17:12:46 -0500

    D&C more

--- doit2_debug.sh
+++ doit2_debug.sh
@@ -4,16 +4,16 @@
 #sbatch job_launch2.sh data0
 
 #sbatch job_launch2.sh data1
-sbatch job_launch2.sh data1-30
-sbatch job_launch2.sh data1-31
-sbatch job_launch2.sh data1-32
-sbatch job_launch2.sh data1-33
-sbatch job_launch2.sh data1-34
-sbatch job_launch2.sh data1-35
-sbatch job_launch2.sh data1-36
-sbatch job_launch2.sh data1-37
-sbatch job_launch2.sh data1-38
-sbatch job_launch2.sh data1-39
+sbatch job_launch2.sh data1-390
+sbatch job_launch2.sh data1-391
+sbatch job_launch2.sh data1-392
+sbatch job_launch2.sh data1-393
+sbatch job_launch2.sh data1-394
+sbatch job_launch2.sh data1-395
+sbatch job_launch2.sh data1-396
+sbatch job_launch2.sh data1-397
+sbatch job_launch2.sh data1-398
+sbatch job_launch2.sh data1-399
 
 #sbatch job_launch2.sh data2
 
