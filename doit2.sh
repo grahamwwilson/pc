078edaf3c8e28779d69bb663e4426fb067dd554d
graham <Graham.Wilson@cern.ch>
2020-06-06 16:25:46 -0500

    new lists

--- doit2.sh
+++ doit2.sh
@@ -2,7 +2,19 @@
 
 # 3 data jobs
 sbatch job_launch2.sh data0
-sbatch job_launch2.sh data1
+
+#sbatch job_launch2.sh data1
+sbatch job_launch2.sh data1-0
+sbatch job_launch2.sh data1-1
+sbatch job_launch2.sh data1-2
+sbatch job_launch2.sh data1-3
+sbatch job_launch2.sh data1-4
+sbatch job_launch2.sh data1-5
+sbatch job_launch2.sh data1-6
+sbatch job_launch2.sh data1-7
+sbatch job_launch2.sh data1-8
+sbatch job_launch2.sh data1-9
+
 sbatch job_launch2.sh data2
 
 # 3 MC jobs
